7b5982606c395e84cf0c9218e5e3373c58e3df5d
Jeppe Schultz Nielsen <Jeppe-schultz@hotmail.com>
2023-04-19 13:26:59 +0200

    c in mc integration

--- homework/montecarlointegration/main.cs
+++ homework/montecarlointegration/main.cs
@@ -7,8 +7,14 @@ using System.IO;
 public static class main{
 
 	public static double circle(vector x){
-		if(x[0]*x[0]+x[1]*x[1] < 1) return 1;
+		if(x[0]*x[0]+x[1]*x[1] <= 1) return 1;
 		else return 0;
+	}
+
+	public static double heavy(vector x){
+		if(x[0] <= 0.8) return 1;
+		if(x[1] <= 0.8) return 1;
+		return 0;
 	}
 
 	public static void Main(){
@@ -115,6 +121,7 @@ public static class main{
 		WriteLine("In plain Monte Carlo, going from 1k to 100k samples improved precision by a factor 10, with Halton sequence its almost a factor 100.");
 		WriteLine($"");
 		WriteLine("Now I will solve the integrals with stratified sampling. THIS PART DOES NOT WORK.");
+		/*
 		WriteLine($"");
 		WriteLine("Calculating some test integrals with stratified Monte Carlo integrator with 1000 points: ");
 		a = new vector(2);
@@ -123,27 +130,50 @@ public static class main{
 		b[1] = PI;
 		a[0] = 0;
 		a[1] = 0;
-		(res, err) = mcint.stratifiedint(x => x[0]*x[0]*Sin(x[1]), a, b, 1000,100);
+		(res, err) = mcint.strataint(x => x[0]*x[0]*Sin(x[1]), a, b, 1000,100);
+		WriteLine($"Int( r^2sin(theta) ) with r from 0 to 1 and theta from 0 to 2pi is calculated as: {res} +- {err}" );
+		WriteLine($"It should be 1/(2pi) * 4/3 * pi * 1^3 = 2/3 = {2.0/3.0}");
+		(res, err) = mcint.strataint(x => x[0]*x[0]*Sin(x[1]), a, b, 10000,100);
+		WriteLine($"Int( r^2sin(theta) ) with r from 0 to 1 and theta from 0 to 2pi is calculated as: {res} +- {err}" );
+		WriteLine($"It should be 1/(2pi) * 4/3 * pi * 1^3 = 2/3 = {2.0/3.0}");
+		(res, err) = mcint.strataint(x => x[0]*x[0]*Sin(x[1]), a, b, 100000,100);
 		WriteLine($"Int( r^2sin(theta) ) with r from 0 to 1 and theta from 0 to 2pi is calculated as: {res} +- {err}" );
 		WriteLine($"It should be 1/(2pi) * 4/3 * pi * 1^3 = 2/3 = {2.0/3.0}");
 		b[0] = 1;
 		b[1] = 1;
 		a[0] = 0;
 		a[1] = 0;
-		(res, err) = mcint.stratifiedint(x => (Exp( - (x[0]*x[0] + x[1]*x[1])) ), a, b, 1000,100);
+		(res, err) = mcint.strataint(x => (Exp( - (x[0]*x[0] + x[1]*x[1])) ), a, b, 1000,100);
 		WriteLine($"Int( e^(x^2+y^2) ) with x from 0 to 1 and y from 0 to 1 is calculated as: {res} +- {err}");
 		WriteLine($"It should be 0.557746");
 		WriteLine($"");
-		WriteLine("Calculating some test integrals with stratified Monte Carlo integrator with 100000 points: ");
+		WriteLine("Calculating some test integrals with stratified Monte Carlo integrator with 1000 points: ");*/
 		b[0] = 1;
 		b[1] = 1;
-		a[0] = -1;
-		a[1] = -1;
-		(res, err) = mcint.stratifiedint(circle, a, b, 100000,100);
-		WriteLine($"Integrating a circle of radius 1 and height 1 with stratified sampling with x and y from -1 to 1 is calculated as: {res} +- {err}");
+		a[0] = 0;
+		a[1] = 0;
+		string path = "test.txt";
+		using (StreamWriter sw = File.CreateText(path)){}
+		(res, err) = mcint.strataint(circle, a, b, 100000,1000,path);
+		WriteLine($"Integrating a circle of radius 1 and height 1 with stratified sampling with x and y from 0 to 1 is calculated as: {res} +- {err} with 100000 points");
 		(res, err) = mcint.plainmc(circle, a, b, 100000);
-		WriteLine($"Integrating a circle of radius 1 and height 1 with plain sampling with x and y from -1 to 1 is calculated as: {res} +- {err}");
-		WriteLine($"It should be pi, {PI}");
-
+		WriteLine($"Integrating a circle of radius 1 and height 1 with plain sampling with x and y from 0 to 1 is calculated as: {res} +- {err} with 100000 points");
+		(res, err) = mcint.strataint(circle, a, b, 1000000,10000);
+		WriteLine($"Integrating a circle of radius 1 and height 1 with stratified sampling with x and y from 0 to 1 is calculated as: {res} +- {err} with 1e6 points");
+		(res, err) = mcint.plainmc(circle, a, b, 1000000);
+		WriteLine($"Integrating a circle of radius 1 and height 1 with plain sampling with x and y from 0 to 1 is calculated as: {res} +- {err} with 1e6 points");
+		WriteLine($"It should be pi/4, {PI/4}");
+		WriteLine($"A comparison of convergence has been saved to a plot.");
+		string toWrite = "";
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 5; j++){
+				int N = 10000*(2*j+1)*((int)Pow(10,i));
+				(double resPlain, double errPlain) = mcint.plainmc(circle, a, b, N);
+				(double resHalton, double errHalton) = mcint.haltonint(circle, a, b, N);
+				(double resStrata, double errStrata) = mcint.strataint(circle, a, b, N,(int)N/100);
+				toWrite += $"{N}\t{errPlain}\t{errHalton}\t{errStrata}\n";
+			}
+		}
+		File.WriteAllText("convergence.txt", toWrite);
 	}
 }
--- homework/montecarlointegration/mcint.cs
+++ homework/montecarlointegration/mcint.cs
@@ -2,11 +2,18 @@ using System;
 using System.Threading;
 using static System.Console;
 using static System.Math; 
+using System.IO;
 
 public static class mcint{
     public static Random rnd = new Random();
 
-    public static (double,double) plainmc(Func<vector,double> f,vector a,vector b,int N){
+    public static (double,double) plainmc(Func<vector,double> f,vector a,vector b,int N, string diagString = null){
+        if(N == 0){
+            //this must mean function has constant value here; just return some number with zero uncertainty
+            
+            return (plainmc(f,a,b,1,diagString).Item1,0);
+        }
+
         int dim=a.size; double V=1; 
         for(int i=0;i<dim;i++)V*=b[i]-a[i];
 
@@ -15,10 +22,16 @@ public static class mcint{
 	    var x=new vector(dim);
 
         for(int i=0;i<N;i++){
+            if(!(diagString==null)){
+                using (StreamWriter sw = File.AppendText(diagString))
+                    {
+                        sw.WriteLine($"{x[0]}\t{x[1]}");
+                    }	
+            }
             for(int k=0;k<dim;k++)x[k]=a[k]+rnd.NextDouble()*(b[k]-a[k]);
             double fx=f(x); sum+=fx; sum2+=fx*fx;
         }
-
+        
         double mean=sum/N, sigma=Sqrt(sum2/N-mean*mean);
         var result=(mean*V,sigma*V/Sqrt(N));
         return result;
@@ -73,69 +86,107 @@ public static class mcint{
         return xs;
     }
 
-    public static (double,double) stratifiedint(Func<vector,double> f,vector a,vector b,int N, int nmin){
-        if(N <= nmin){
-            WriteLine("nmin call");
-            return plainmc(f,a,b,N);
+    public static (double,double) strataint(Func<vector,double> f,vector a,vector b,int N, int nmin, string diagString = null){
+        if(N < nmin){
+            return plainmc(f,a,b,N,diagString);
         }
-        else{
-            //calculate, for each dimension, the variance in two halves of its area. Select the highest variance.
-            int dimmax = 0;
-            double errmax = 0;
-            double lowerErr = 0;
-            double upperErr = 0;
-            double cmax = 0;
-            int dim = a.size;
-            for(int i = 0; i < dim; i++){
-                double olda = a[i];
-                double oldb = b[i];
-                double newc = a[i] + 0.5 * (b[i]-a[i]);
-                //first calculate with upper subvolume of this dimension
-                a[i] = newc;
-                (double res1, double err1) = plainmc(f,a,b,nmin/2);
-                //reset a, calculate with lower subvolume
-                a[i] = olda;
-                b[i] = newc;
-                (double res2, double err2) = plainmc(f,a,b,nmin/2);
-                //reset b
-                b[i] = oldb;
-                if(err1 > errmax){
-                    errmax = err1;
-                    dimmax = i;
-                    cmax = newc;
-                    upperErr = err1;
-                    lowerErr = err2;
+
+        int dim = a.size;
+        var x=new vector(dim);
+        vector leftn = new vector(dim), rightn = new vector(dim), rightf = new vector(dim), leftf = new vector(dim), rightf2 = new vector(dim), leftf2 = new vector(dim);
+        vector leftvar = new vector(dim), rightvar = new vector(dim), leftmean = new vector(dim), rightmean = new vector(dim);
+
+        double V=1; 
+        for(int i=0;i<dim;i++)V*=b[i]-a[i];
+
+        for(int i = 0; i < nmin; i++){
+            for(int k=0;k<dim;k++)x[k]=a[k]+rnd.NextDouble()*(b[k]-a[k]);
+            //WriteLine($"{x[0]}, {x[1]},");
+            double fx=f(x);
+            if(!(diagString==null)){
+                using (StreamWriter sw = File.AppendText(diagString))
+                    {
+                        sw.WriteLine($"{x[0]}\t{x[1]}");
+                    }	
+            }
+            for(int j = 0; j < dim; j++){
+                if(x[j] > (a[j] + 0.5 * (b[j]-a[j]))){
+                    //right half in this dimension.
+                    rightn[j]++;
+                    rightf[j]+=fx;
+                    rightf2[j]+=fx*fx;
                 }
-                if(err2 > errmax){
-                    errmax = err2;
-                    dimmax = i;
-                    cmax = newc;
-                    upperErr = err1;
-                    lowerErr = err2;
+                else{
+                    //left half 
+                    leftn[j]++;
+                    leftf[j]+=fx;
+                    leftf2[j]+=fx*fx;
                 }
             }
-            //I now know the dimension which dimension has volume with largest sub-variance. Split points weighted among volumes
-            WriteLine(upperErr);
-            WriteLine(lowerErr);
-            int Nupper = (int)Round(N * upperErr/(lowerErr+upperErr));
-            int Nlower = N-Nupper;
-            WriteLine(Nupper);
-            WriteLine(Nlower);
-            WriteLine(N);
-            //calculate mean and variance in sub-volumes
-            double oldamax = a[dimmax];
-            double oldbmax = b[dimmax];
-            a[dimmax] = cmax;
-            var result1 = stratifiedint(f, a, b, Nupper, nmin);
-            a[dimmax] = oldamax;
-            b[dimmax] = cmax;
-            var result2 = stratifiedint(f, a, b, Nlower, nmin);
-            WriteLine("Results:");
-            WriteLine(result1.Item1);
-            WriteLine(result2.Item1);
-            b[dimmax] = oldbmax;
-            double mean = result1.Item1 + result2.Item1, sigma = Sqrt(result1.Item2 * result1.Item2 + result2.Item2 * result2.Item2);
-            return (mean, sigma);
         }
+        double maxvar = 0;
+        int dimmax = 0;
+//
+        for(int i = 0; i < dim; i++){
+            leftmean[i] = leftf[i]/leftn[i];
+            rightmean[i] = rightf[i]/rightn[i];
+            leftvar[i] = Sqrt(leftf2[i]/leftn[i] - leftmean[i]*leftmean[i])*V/2/Sqrt(leftn[i]);
+            rightvar[i] = Sqrt(rightf2[i]/rightn[i] - rightmean[i]*rightmean[i])*V/2/Sqrt(rightn[i]);
+            double dvar = Abs(rightmean[i] - leftmean[i]);
+            if(dvar > maxvar){
+                maxvar = dvar;
+                dimmax = i;
+            }
+        }
+        //dimension of maximal variance from this level is subdivided; put N - nmin on each side in weighted manner
+        int Nleft = (int)((N-nmin) * Abs(leftvar[dimmax])/(Abs(leftvar[dimmax])+Abs(rightvar[dimmax])));
+        int Nright = (N-nmin)-Nleft;
+
+        double cmax = a[dimmax] + 0.5 * (b[dimmax]-a[dimmax]);
+        var newa = a.copy();
+        var newb = b.copy();
+        newa[dimmax] = cmax;
+        newb[dimmax] = cmax;
+
+        (double resLeft, double errLeft) = strataint(f, a, newb, Nleft, nmin, diagString);
+        (double resRight, double errRight) = strataint(f, newa, b, Nright, nmin, diagString);
+
+        //result from this level is
+        double integThis = (leftf[dimmax]+rightf[dimmax])/nmin * V;
+        double varThis = Sqrt(Pow(rightvar[dimmax],2) + Pow(leftvar[dimmax],2));
+        /*WriteLine("Start");
+        WriteLine($"Borders x: {a[0]} {b[0]}");
+        WriteLine($"Borders y: {a[1]} {b[1]}");
+        WriteLine($"Cut along axis {dimmax}");
+        WriteLine($"Points left {N}");
+        WriteLine($"Nleftratio {Abs(leftvar[dimmax])/(Abs(leftvar[dimmax])+Abs(rightvar[dimmax]))}");
+        WriteLine($"Dispatch Nleft {Nleft}");
+        WriteLine($"Dispatch Nright {Nright}");
+        WriteLine($"Left n in maxdim {leftn[dimmax]}");
+        WriteLine($"Right n in maxdim {rightn[dimmax]}");
+        WriteLine($"leftvarx {leftvar[0]}");
+        WriteLine($"rightvarx {rightvar[0]}");
+        WriteLine($"leftvary {leftvar[1]}");
+        WriteLine($"rightvary {rightvar[1]}");
+        WriteLine($"leftf2/leftn in dimmax {leftf2[dimmax]/leftn[dimmax]}");
+        WriteLine($"leftmean^2 in dimmax {leftmean[dimmax]*leftmean[dimmax]}");
+        WriteLine($"rightf2/rightn in dimmax {rightf2[dimmax]/rightn[dimmax]}");
+        WriteLine($"rightmean^2 in dimmax {rightmean[dimmax]*rightmean[dimmax]}");
+        WriteLine($"integ right { (rightf[dimmax])/nmin * V }");
+        WriteLine($"integ left { (leftf[dimmax])/nmin * V }");
+        WriteLine(integThis);
+        WriteLine($"from next level we got in right {resRight} after calling with N = {Nright}");
+        WriteLine($"from next level we got in left {resLeft} after calling with N = {Nleft}");
+        WriteLine($"");*/
+
+        //result from next level is
+        double integNext = resLeft + resRight;
+        double varNext = Sqrt(Pow(errLeft,2) + Pow(errRight,2));
+
+        //total result is weighted by number used to calculate this level and next level:
+        double res = ((N-nmin)*integNext + nmin*integThis)/(N);
+        double var = 1.0/N * Sqrt(Pow( (N-nmin)*varNext ,2) + Pow( nmin*varThis ,2));
+
+        return (res,var);
     }
 }
