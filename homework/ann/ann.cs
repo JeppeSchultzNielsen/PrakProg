efa95b96cf731776c495aa420e11049a058de62c
Jeppe Schultz Nielsen <Jeppe-schultz@hotmail.com>
2023-05-03 13:27:01 +0200

    ann might work

---
+++ homework/ann/ann.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Threading;
+using static System.Console;
+using static System.Math; 
+using System.IO;
+
+public class ann{
+    public int n; /* number of hidden neurons */
+    public Func<double,double> f = x => x*Exp(-x*x); /* activation function */
+    public vector p; /* network parameters */
+    public ann(int n){
+        this.n = n; 
+        int numParams = 3*n; //n a_i, n b_i, n w's
+        p = new vector(numParams);
+    }
+    public double response(double x){
+        double result = 0;
+        for(int i = 0; i < n; i++){
+            double a_i = p[i];
+            double b_i = p[n+i];
+            double w_i = p[2*n+i];
+            result += w_i * f( (a_i-x)/b_i );
+        }
+        return result;
+    }
+
+    public double antiderivative(double x){
+        //antiderivative of gaussian wavelet (x-a)/b * exp( ((x-a)/b)^2 ) is -b/2 * exp( ((x-a)/b)^2 )
+        double result = 0;
+        for(int i = 0; i < n; i++){
+            double a_i = p[i];
+            double b_i = p[n+i];
+            double w_i = p[2*n+i];
+            result += w_i*b_i/2.0 * Exp( -Pow((a_i-x)/b_i,2) );
+        }
+        return result;
+    }
+
+    public double derivative(double x){
+        //derivative of gaussian wavelet (x-a)/b * exp( ((x-a)/b)^2 ) is (2*(a-x)^2-b^2)/b^3 * exp( ((x-a)/b)^2 )
+        double result = 0;
+        for(int i = 0; i < n; i++){
+            double a_i = p[i];
+            double b_i = p[n+i];
+            double w_i = p[2*n+i];
+            result += w_i* (2*(a_i-x)*(a_i-x)-b_i*b_i)/Pow(b_i,3) * Exp( -Pow((a_i-x)/b_i,2) );
+        }
+        return result;
+    }
+
+    public double derivative2(double x){
+        //derivative of gaussian wavelet (x-a)/b * exp( ((x-a)/b)^2 ) is 2*(a-x)*(2*(a-x)^2-3*b^2)/b^3 * exp( ((x-a)/b)^2 )
+        double result = 0;
+        for(int i = 0; i < n; i++){
+            double a_i = p[i];
+            double b_i = p[n+i];
+            double w_i = p[2*n+i];
+            result += w_i* (2*(a_i-x))*(2*(a_i-x)*(a_i-x)-b_i*b_i)/Pow(b_i,3) * Exp( -Pow((a_i-x)/b_i,2) );;
+        }
+        return result;
+    }
+    
+    public void train(vector x,vector y){
+        double minx = double.MaxValue;
+        double maxx = double.MinValue;
+        for(int i = 0; i < x.size; i++){
+            if(x[i] > maxx) maxx = x[i];
+            if(x[i] < minx) minx = x[i];
+        }
+
+
+        for(int i = 0; i < p.size; i++){
+            //up to n is the a_i, they should be placed evenly across the interval. 
+            if(i < n) p[i] = minx + (maxx-minx)*i/n;
+            else p[i] = 1; 
+        }
+
+
+        Func<vector,double> c = ps => {
+            double toMin = 0; 
+            for(int j = 0; j < x.size; j++){
+                double result = 0; 
+                for(int i = 0; i < n; i++){
+                    double a_i = ps[i];
+                    double b_i = ps[n+i];
+                    double w_i = ps[2*n+i];
+                    result += w_i * f( (a_i-x[j])/b_i );
+                }
+                toMin += Pow(result-y[j],2);
+            }
+            return toMin/n;
+        };
+        p = minimizer.qnewton(c,p,0.001);
+    }
+}
---
+++ homework/ann/main.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Threading;
+using static System.Console;
+using static System.Math; 
+using System.IO;
+
+public static class main{
+
+	public static double g(double x){
+		return Cos(5*x-1)*Exp(-x*x);
+	}
+
+	public static double sin(double x){
+		return Sin(x);
+	}
+
+	public static void Main(){
+		WriteLine("Part A");
+		//create training set 
+		int dataSize = 20;
+		vector xs = new vector(dataSize);
+		vector ys = new vector(dataSize);
+
+		WriteLine("Making training points..");
+		string toWrite = "";
+		for(int i = 0; i < dataSize; i++){
+			double x = -1 + 2.0 * i / dataSize;
+			xs[i] = x;
+			ys[i] = g(x);
+			toWrite += $"{xs[i]}\t{ys[i]}\n";
+		}
+		File.WriteAllText("txts/trainingPoints.txt",toWrite);
+		WriteLine("Making interpolation with 7 neurons...");
+		ann myAnn = new ann(7);
+		myAnn.train(xs,ys);
+
+		//create interpolation set
+		toWrite = "";
+		int interpolationPoints = 1000;
+		for(int i = 0; i < interpolationPoints; i++){
+			double x = -1 + 2.0 * i / interpolationPoints;
+			toWrite += $"{x}\t{myAnn.response(x)}\n";
+		}
+		File.WriteAllText("txts/7neuronInterp.txt",toWrite);
+
+		WriteLine("Making interpolation with 20 neurons...");
+		myAnn = new ann(20);
+		myAnn.train(xs,ys);
+		//create interpolation set
+		toWrite = "";
+		for(int i = 0; i < interpolationPoints; i++){
+			double x = -1 + 2.0 * i / interpolationPoints;
+			toWrite += $"{x}\t{myAnn.response(x)}\n";
+		}
+		File.WriteAllText("txts/20neuronInterp.txt",toWrite);
+		WriteLine("Interpolation can be seen in Interpolation.svg");
+		WriteLine("");
+		WriteLine("Part B");
+		WriteLine("Figures have been made. Both with Cos(5x-1)*exp(-x^2) and sin(2x).");
+		toWrite = "";
+		for(int i = 0; i < interpolationPoints; i++){
+			double x = -1 + 2.0 * i / interpolationPoints;
+			toWrite += $"{x}\t{myAnn.antiderivative(x)}\t{myAnn.derivative(x)}\t{myAnn.derivative2(x)}\n";
+		}
+		File.WriteAllText("txts/derivatives.txt",toWrite);
+
+		dataSize = 100;
+		xs = new vector(dataSize);
+		ys = new vector(dataSize);
+		toWrite = "";
+		for(int i = 0; i < dataSize; i++){
+			double x = 2.0*PI * i / dataSize;
+			xs[i] = x;
+			ys[i] = sin(x);
+			toWrite += $"{x}\t{sin(x)}\n";
+		}
+		File.WriteAllText("txts/sinTraining.txt",toWrite);
+
+		myAnn = new ann(10);
+		myAnn.train(xs,ys);
+		toWrite = "";
+		for(int i = 0; i < interpolationPoints; i++){
+			double x = 2.0*PI * i / interpolationPoints;
+			toWrite += $"{x}\t{myAnn.antiderivative(x)}\t{myAnn.derivative(x)}\t{myAnn.derivative2(x)}\t{myAnn.response(x)}\n";
+		}
+		File.WriteAllText("txts/sinDerivatives.txt",toWrite);
+
+	}
+}
