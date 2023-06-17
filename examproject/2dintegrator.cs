14f63e1e604d79bcfec8e0fe07f7cb585dbc9050
jeppe <201908734@post.au.dk>
2023-06-17 13:15:52 +0200

    Start C exam

--- examproject/2dintegrator.cs
+++ examproject/2dintegrator.cs
@@ -5,39 +5,48 @@ using static System.Console;
 public static class integrator2d{
     
 
-    public static double integ2Drectangle(
+    public static (double,int,int) integ2Drectangle(
         Func<double,double,double> f,
         double a, double b,
         double c, double d,
         double acc =1e-5, double eps=1e-5)
         {
+        int nEvals = 0;
+        int nBoundEvals = 0;
         Func<double,double> g = x => {
             Func<double,double> integHere = y => {
                 return f(x,y);
             };
             (double resG, double errG) = integrator.integrateGeneral(integHere,c,d,acc,eps);
+            nEvals += integrator.nEvals;
+            nBoundEvals++;
             return resG;
         };
         (double res, double err) = integrator.integrateGeneral(g,a,b,acc,eps);
-        return res;
+        
+        return (res, nEvals, nBoundEvals);
     }
 
-    public static double integ2D(
+    public static (double,int,int) integ2D(
         Func<double,double,double> f,
         double a, double b,
         Func<double,double> d,
         Func<double,double> u,
         double acc =1e-5, double eps=1e-5)
         {
+        int nEvals = 0;
+        int nBoundEvals = 0;
         Func<double,double> g = x => {
             Func<double,double> integHere = y => {
                 return f(x,y);
             };
             (double resG, double errG) = integrator.integrateGeneral(integHere,d(x),u(x),acc,eps);
+            nEvals += integrator.nEvals;
+            nBoundEvals++;
             return resG;
         };
         (double res, double err) = integrator.integrateGeneral(g,a,b,acc,eps);
-        return res;
+        return (res, nEvals, nBoundEvals);
     }
 
     public static (double,double,int) halton2Dint(Func<double,double,double> f,
@@ -54,7 +63,8 @@ public static class integrator2d{
 	    var x=new vector(dim);
         var x2=new vector(dim);
 
-        int fcalls = 0;
+        int fcalls1 = 0;
+        int fcalls2 = 0;
 
         for(int i=0;i<N;i++){
             vector n1 = halton(i,dim);
@@ -66,23 +76,18 @@ public static class integrator2d{
             x2[0]=a+n2[0]*(b-a);
             x2[1]=ymin+n2[1]*(ymax-ymin);
 
-            /*WriteLine($"x: {x[0]}");
-            WriteLine($"y: {x[1]}");
-            WriteLine($"lower: {d(x[0])}");
-            WriteLine($"upper: {u(x[0])}");*/
-
             if(x[1] > d(x[0]) &&  x[1] < u(x[0])){
-                sum+=f(x[0],x[1]); fcalls++;
-                //WriteLine("Accepted");
+                sum+=f(x[0],x[1]); 
+                fcalls1++;
             }
             if(x2[1] > d(x[0]) &&  x2[1] < u(x[0])){
                 sum2+=f(x2[0],x2[1]); 
-                fcalls++;
+                fcalls2++;
                 
             }
         }
 
-        var result=(sum*V/N,Abs(sum*V/N-sum2*V/N),fcalls);
+        var result=(sum*V/N,Abs(sum*V/N-sum2*V/N),fcalls1+fcalls2);
         return result;
     }
 
--- examproject/main.cs
+++ examproject/main.cs
@@ -14,23 +14,23 @@ public static class main{
 		WriteLine("");
 
 		Func<double, double, double> f = (x,y) => {return 1;};
-		double res = integrator2d.integ2Drectangle(f,-5,5,-5,5);
+		(double res, int nEvals, int boundEvals) = integrator2d.integ2Drectangle(f,-5,5,-5,5);
 		WriteLine($"Integrating f(x,y)=1 with x in (-5,5) and y in (-5,5) gives result {res}, it should be 100");
 		WriteLine("");
 
 		f = (x,y) => {return x*y*y;};
-		res = integrator2d.integ2Drectangle(f,0,2,0,1);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,0,2,0,1);
 		WriteLine($"Integrating f(x,y)=x*y*y with x in (0,2) and y in (0,1) gives result {res}, it should be (1/2*2^2)*(1/3*1^3)=2/3");
 		WriteLine("");
 
 		f = (x,y) => {return Cos(x*y);};
-		res = integrator2d.integ2Drectangle(f,-PI,PI,-PI,PI);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,-PI,PI,-PI,PI);
 		WriteLine($"Integrating f(x,y)=x*y*y with x in (-PI,PI) and y in (-PI,PI) gives result {res}. According to WolframAlpha, it should be Si(pi^2)=6.659");
 		WriteLine("");
 
 		WriteLine("Since the implementation made in the homework allows infinite limits, we can also test if this works in 2d.");
 		f = (x,y) => {return Exp(-x*x-y*y);};
-		res = integrator2d.integ2Drectangle(f,-double.PositiveInfinity,double.PositiveInfinity,-double.PositiveInfinity,double.PositiveInfinity);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,-double.PositiveInfinity,double.PositiveInfinity,-double.PositiveInfinity,double.PositiveInfinity);
 		WriteLine($"Integrating f(x,y)=Exp(-x*x-y*y) with x in (-inf,inf) and y in (-inf,inf) gives result {res}, it should be pi.");
 		WriteLine("");
 
@@ -40,27 +40,27 @@ public static class main{
 		Func<double, double> u = x => {return Sqrt(1-x*x);};
 		Func<double, double> d = x => {return -Sqrt(1-x*x);};
 		f = (x,y) => {return 1;};
-		res = integrator2d.integ2D(f,-1,1,d,u);
+		(res, nEvals, boundEvals) = integrator2d.integ2D(f,-1,1,d,u);
 		WriteLine($"Integrating f(x,y)=1 with x in (-1,1) and y in (-Sqrt(1-x*x),Sqrt(1-x*x)) (a circle of radius 1) gives result {res}");
 		f = (r,v) => {return r;};
-		res = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
 		WriteLine($"In polar coordinates this can be restated as a rectangular integral with x->cos(v), y->sin(v), dxdy -> rdrdv, v in (0,2pi) and r in (0,1).");
 		WriteLine($"This then gives {res}");
 		WriteLine("");
 
 		f = (x,y) => {return x*y;};
-		res = integrator2d.integ2D(f,-1,1,d,u);
+		(res, nEvals, boundEvals) = integrator2d.integ2D(f,-1,1,d,u);
 		WriteLine($"Integrating f(x,y)=x*y with x in (-1,1) and y in (-Sqrt(1-x*x),Sqrt(1-x*x)) (a circle of radius 1) gives result {res}");
 		f = (r,v) => {return r*r*r*Cos(v)*Sin(v);};
-		res = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
 		WriteLine($"The same integral in polar coordinates is ∫∫r²sin(v)cos(v)rdrdv. This gives {res}");
 		WriteLine("");
 
 		f = (x,y) => {return Exp(x)*Exp(y);};
-		res = integrator2d.integ2D(f,-1,1,d,u);
+		(res, nEvals, boundEvals) = integrator2d.integ2D(f,-1,1,d,u);
 		WriteLine($"Integrating f(x,y)=Exp(x)*Exp(y) with x in (-1,1) and y in (-Sqrt(1-x*x),Sqrt(1-x*x)) (a circle of radius 1) gives result {res}");
 		f = (r,v) => {return Exp(r*(Cos(v)+Sin(v)))*r;};
-		res = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
+		(res, nEvals, boundEvals) = integrator2d.integ2Drectangle(f,0,1,0,2*PI);
 		WriteLine($"The same integral in polar coordinates is ∫∫exp( r*(sin(v)cos(v)) )rdrdv. This gives {res}");
 		WriteLine("");
 
@@ -68,7 +68,7 @@ public static class main{
 		f = (x,y) => {return Exp(-x*x-y*y);};
 		u = x => {return Exp(-x*x);};
 		d = x => {return -Exp(-x*x);};
-		res = integrator2d.integ2D(f,-double.PositiveInfinity,double.PositiveInfinity,d,u);
+		(res, nEvals, boundEvals) = integrator2d.integ2D(f,-double.PositiveInfinity,double.PositiveInfinity,d,u);
 		WriteLine($"Integrating f(x,y)=Exp(-x*x-y*y) with x in (-inf,inf) and y in (-Exp(-x*x),Exp(x*x)) gives result {res}, according to WolframAlpha it is 2.03519");
 		WriteLine("");
 
@@ -77,13 +77,25 @@ public static class main{
 		WriteLine("The Halton-sequence integrator of homework 7 has been extended to take arguments similar to the adaptive 2d integrator.");
 		WriteLine("The code is heavily inspired by that of homework 7.");
 		WriteLine("The size of the sampling box is given by the user.");
+		WriteLine("");
+
 		WriteLine("Points will be sampled within the box. If they are not within the integration boundary, they are discarded.");
 		WriteLine("Testing this with the integral f(x,y)=Exp(x)*Exp(y) with x in (-1,1) and y in (-Sqrt(1-x*x),Sqrt(1-x*x)) (a circle of radius 1) with 10k points");
 
-		f = (r,v) => {return Exp(r*(Cos(v)+Sin(v)))*r;};
+		f = (x,y) => {return Exp(x)*Exp(y);};
 		u = x => {return Sqrt(1-x*x);};
 		d = x => {return -Sqrt(1-x*x);};
 		(double resHalton, double errHalton, int fcalls) = integrator2d.halton2Dint(f,-1,1,-1,1,d,u,10000);
 		WriteLine($"The result is {resHalton}+-{errHalton}, with {fcalls} calls to f. The boundaries functions have each been evaluated 20k times.");
+		WriteLine("");
+
+		WriteLine("The 2dintegrator has been modified to return number of steps. We can now check whether the Clenshaw-Curtis transformation helps in 2d.");
+		WriteLine("To do this, we calculate ∫∫ln(x)*ln(y)/sqrt(x*y)dxdy with x and y from 0 to 1.");
+		f = (x,y) => {return 1/Sqrt(x*y);};
+		u = x => {return 1;};
+		d = x => {return 0;};
+		(res, nEvals, boundEvals) = integrator2d.integ2D(f,0,1,d,u);
+		WriteLine($"This results in {res} after {nEvals} integration calls (f is called twice in each) and {boundEvals} calls to the functions defining the boundaries.");
+
 	}
 }
